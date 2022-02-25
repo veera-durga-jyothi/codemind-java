d82b25fec4b5e4050b69e6ffe140b537a1e7ba98
veera-durga-jyothi <69592088+veera-durga-jyothi@users.noreply.github.com>
2022-02-25 20:34:39 +0530

    import java.util.*; class Input {     public static void main(String[] args)     {         Scanner s=new Scanner(System.in);         String n=s.nextLine();         System.out.print(n+"");     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Input
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        String n=s.nextLine();
+        System.out.print(n+"");
+    }
+}
