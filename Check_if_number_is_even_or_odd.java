4c76aa3d6dee56756867053b7fbcb6a6bb29ae48
veera-durga-jyothi <69592088+veera-durga-jyothi@users.noreply.github.com>
2022-02-26 15:36:34 +0530

    import java.util.*; class Evenodd {     public static void main(String[] args)     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         if(n%2==0)         {             System.out.println("Even");         }         else         {             System.out.println("Odd");         }     } }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Evenodd
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("Odd");
+        }
+    }
+}
