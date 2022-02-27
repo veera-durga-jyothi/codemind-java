10691420b503a60a0eb2e67058202a9a841d33d5
veera-durga-jyothi <69592088+veera-durga-jyothi@users.noreply.github.com>
2022-02-27 18:27:04 +0530

    import java.util.*; class Sumofnaturalno {     public static void main(String[] args)     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a=n*(n+1)/2;         System.out.println(a);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Sumofnaturalno
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a=n*(n+1)/2;
+        System.out.println(a);
+    }
+}
