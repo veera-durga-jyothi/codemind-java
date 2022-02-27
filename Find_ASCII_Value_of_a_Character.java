4f2e746c789d3ba61fe776cd304fc2f414d4f3a7
veera-durga-jyothi <69592088+veera-durga-jyothi@users.noreply.github.com>
2022-02-27 14:45:33 +0530

    import java.util.*; class Ascill {     public static void main(String[] args)     {         Scanner s=new Scanner(System.in);         char ch=s.next().charAt(0);         int i=ch;         System.out.println(i);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Ascill
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        char ch=s.next().charAt(0);
+        int i=ch;
+        System.out.println(i);
+    }
+}
